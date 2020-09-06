c60f8dc6328775f659cca70e265d32c673a04b90
Ryan Littleton <ryan.littleton@mymail.champlain.edu>
2020-09-06 17:32:57 -0400

    Removed auto calls and output to a ppm, correctly shows up in PS

--- RLittleton_Lab1/RLittleton_Lab1/RLittleton_Lab1/Main.cpp
+++ RLittleton_Lab1/RLittleton_Lab1/RLittleton_Lab1/Main.cpp
@@ -1,6 +1,9 @@
 // This is a placeholder to test that everything is set up correctly
 
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 int main() {
 
@@ -11,19 +14,26 @@ int main() {
 
     // Render
 
-    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+    ofstream output;
+    output.open("out.ppm");
+
+    output << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
         for (int i = 0; i < image_width; ++i) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.25;
+            double r = double(i) / (double(image_width) - 1); // Casting these to doubles to avoid warnings about 4 and 6 bit overflows.
+            double g = double(j) / (double(image_height) - 1);
+            double b = 0.25;
 
             int ir = static_cast<int>(255.999 * r);
             int ig = static_cast<int>(255.999 * g);
             int ib = static_cast<int>(255.999 * b);
 
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            output << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    output.close();
+
+   return 0;
 }
