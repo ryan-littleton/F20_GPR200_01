00a35c4cdd6779e527a87bd9ba7cc48d9bbd27fc
Ryan Littleton <ryan.littleton@mymail.champlain.edu>
2020-09-10 19:47:54 -0400

    Main adjustments and out works

--- RLittleton_Lab1/RLittleton_Lab1/RLittleton_Lab1/Main.cpp
+++ RLittleton_Lab1/RLittleton_Lab1/RLittleton_Lab1/Main.cpp
@@ -1,4 +1,16 @@
-// This is a placeholder to test that everything is set up correctly
+/*
+Author: Ryan Littleton
+Class : GPR-200-01
+Assignment : Lab 1
+*/
+/*
+    https://raytracing.github.io/books/RayTracingInOneWeekend.html#overview
+    Raytracing tutorial for Lab 1, made by Peter Shirley
+
+    Modified by: Ryan Littleton
+    Modified because: Following the Raytracing in a Weekend tutorial from the link above.
+*/
+
 
 #include <iostream>
 #include <fstream>
@@ -12,14 +24,15 @@ int main() {
     const int image_width = 256;
     const int image_height = 256;
 
-    // Render
-
-    ofstream output;
+    // Initialize output file
+    ofstream output; 
     output.open("out.ppm");
 
+    // Render
     output << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = image_height - 1; j >= 0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
             double r = double(i) / (double(image_width) - 1); // Casting these to doubles to avoid warnings about 4 and 6 bit overflows.
             double g = double(j) / (double(image_height) - 1);
@@ -33,7 +46,7 @@ int main() {
         }
     }
 
+    std::cerr << "\nDone.\n";
     output.close();
 
-   return 0;
 }
