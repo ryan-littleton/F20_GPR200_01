d28a027eb2e2d7d77ad559425000f2c0466981f1
Ryan Littleton <ryan.littleton@mymail.champlain.edu>
2020-11-04 21:28:53 -0500

    normal to sphere color

--- RLittleton_Lab7/shaders/HelloGLSLPS.glsl
+++ RLittleton_Lab7/shaders/HelloGLSLPS.glsl
@@ -1,1 +1,34 @@
-// empty shader file
+#version 330
+
+/*
+Author: Ryan Littleton - using class code tutorial from Daniel Buckstein
+Class : GPR-200-01
+Assignment : Lab 7
+*/
+
+#ifdef GL_ES
+precision highp float;
+#endif // GL_ES
+
+layout (location = 0) out vec4 rtFragColor;
+
+// Varying
+// Per-Vertex: receive final color
+// in vec4 color;
+
+// Per-Fragment: receive reqs for final color
+in vec4 vNormal;
+in vec2 vTexcoord;
+
+void main() 
+{
+	//rtFragColor = vec4(1.0);
+	
+	// Per-Vertex input is final color
+	// rtFragColor = vColor;
+	
+	// Per-Fragment inputs to calc final color
+	vec4 N = normalize(vNormal);
+	rtFragColor = vec4(N.xyz * 0.5 + 0.5, 1.0);
+	//rtFragColor = vec4(vTexcoord, 0.0, 1.0);
+}
--- RLittleton_Lab7/shaders/HelloGLSLVS.glsl
+++ RLittleton_Lab7/shaders/HelloGLSLVS.glsl
@@ -1,1 +1,73 @@
-// empty shader file
+#version 330
+
+/*
+Author: Ryan Littleton - using class code tutorial from Daniel Buckstein
+Class : GPR-200-01
+Assignment : Lab 7
+*/
+
+// Main duty: read attributes
+// 3D position in space
+// uv: texture coordinate
+// normal
+
+// Object Space
+layout (location = 0) in vec4 aPosition;
+layout (location = 1) in vec3 aNormal;
+
+// Texture Space
+layout (location = 2) in vec2 aTexcoord;
+
+// Transform Uniforms
+uniform mat4 uModelMat;
+uniform mat4 uViewMat;
+uniform mat4 uProjMat;
+uniform mat4 uViewProjMat;
+
+// Varying
+
+// Per-Vertex: Final Color
+// out vec4 vColor;
+
+// Per-Fragment: indiv. components
+out vec4 vNormal;
+out vec2 vTexcoord;
+
+void main()
+{
+	// Required: Set gl_Position
+	// problem: gl_position is in clip space
+	// problem: aPosition is in object space
+	// gl_Position = aPosition NO
+	
+	// position in world space (still no)
+	// vec4 pos_world = uModelMat * aPosition;
+	// gl_Position = pos_world;
+	
+	// position in camera space (still no)
+	// vec4 pos_camera = uViewMat * pos_world;
+	// gl_Position = pos_camera;
+	
+	// position in clip space (yay)
+	// vec4 pos_clip = uViewProjMat * pos_world;
+	
+	// Final Pos Pipeline
+	mat4 modelViewMat = uViewMat * uModelMat;
+	vec4 pos_camera = modelViewMat * aPosition;
+	vec4 pos_clip = uProjMat * pos_camera;
+	gl_Position = pos_clip;
+	
+	// Normal Pipeline
+	mat3 normalMatrix = transpose(inverse(mat3(modelViewMat)));
+	vec3 norm_camera = normalMatrix * aNormal;
+	
+	// Optionally write varyings
+	// vColor = aPosition; etc.
+	
+	// Per-Fragment outputs
+	vNormal = vec4(norm_camera, 0.0);
+	vTexcoord = aTexcoord;
+	
+	
+
+}
